25b6d14afe7574c755b124145dcfd95d9b07e607
Natalia Borbon <nattycb1608@gmail.com>
2025-05-23 22:52:07 -0300

    Agregado archivo edu_ciaa.h

---
+++ inc/edu-ciaa.h
@@ -0,0 +1,98 @@
+/*********************************************************************************************************************
+Copyright (c) 2025, Natalia Carolina Borbón <nataliacborbon@gmail.com>
+Copyright (c) 2025, Laboratorio de Microprocesadores, Universidad Nacional de Tucumán, Argentina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*********************************************************************************************************************/
+
+#ifndef EDU_CIAA_H_
+#define EDU_CIAA_H_
+
+/** @file edu-ciaa.h
+ ** @brief Configuración y control de la placa EDU-CIAA.
+ **/
+
+/* === Headers files inclusions ==================================================================================== */
+
+/* === Header for C++ compatibility ================================================================================ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Public macros definitions =================================================================================== */
+
+#define LED_B_PORT 2
+#define LED_B_PIN  2
+#define LED_B_FUNC SCU_MODE_FUNC4
+#define LED_B_GPIO 5
+#define LED_B_BIT  2
+
+#define LED_1_PORT 2
+#define LED_1_PIN  10
+#define LED_1_FUNC SCU_MODE_FUNC0
+#define LED_1_GPIO 0
+#define LED_1_BIT  14
+
+#define LED_2_PORT 2
+#define LED_2_PIN  11
+#define LED_2_FUNC SCU_MODE_FUNC0
+#define LED_2_GPIO 1
+#define LED_2_BIT  11
+
+#define LED_3_PORT 2
+#define LED_3_PIN  12
+#define LED_3_FUNC SCU_MODE_FUNC0
+#define LED_3_GPIO 1
+#define LED_3_BIT  12
+
+#define TEC_1_PORT 1
+#define TEC_1_PIN  0
+#define TEC_1_FUNC SCU_MODE_FUNC0
+#define TEC_1_GPIO 0
+#define TEC_1_BIT  4
+
+#define TEC_2_PORT 1
+#define TEC_2_PIN  1
+#define TEC_2_FUNC SCU_MODE_FUNC0
+#define TEC_2_GPIO 0
+#define TEC_2_BIT  8
+
+#define TEC_3_PORT 1
+#define TEC_3_PIN  2
+#define TEC_3_FUNC SCU_MODE_FUNC0
+#define TEC_3_GPIO 0
+#define TEC_3_BIT  9
+
+#define TEC_4_PORT 1
+#define TEC_4_PIN  6
+#define TEC_4_FUNC SCU_MODE_FUNC0
+#define TEC_4_GPIO 1
+#define TEC_4_BIT  9
+
+/* === Public data type declarations =============================================================================== */
+
+/* === Public variable declarations ================================================================================ */
+
+/* === Public function declarations ================================================================================ */
+
+/* === End of conditional blocks =================================================================================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EDU-CIAA_H_ */
