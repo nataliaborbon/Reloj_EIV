63dc4050141615190a29abf25cd5bd88edf369aa
Natalia Borbon <nattycb1608@gmail.com>
2025-05-23 23:23:31 -0300

    Agregado dee una linea de control en bsp.c

--- src/bsp.c
+++ src/bsp.c
@@ -50,29 +50,31 @@ SPDX-License-Identifier: MIT
 board_t BoardCreate(void) {
     struct board_s * self = malloc(sizeof(struct board_s));
 
-    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
-    self->led_red = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
+    if (self != NULL) {
+        Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
+        self->led_red = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
 
-    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    self->led_green = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT);
+        Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
+        self->led_green = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT);
 
-    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
-    self->led_blue = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
+        Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
+        self->led_blue = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
 
-    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
-    self->led_yellow = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
+        Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
+        self->led_yellow = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
 
-    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
-    self->tec_1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
+        Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
+        self->tec_1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
 
-    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
-    self->tec_2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
+        Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
+        self->tec_2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
 
-    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
-    self->tec_3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
+        Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
+        self->tec_3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
 
-    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
-    self->tec_4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+        Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
+        self->tec_4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+    }
 
     return self;
 }
